088dc0f427b5adde1b73a3fd621fbdbbb63578ab
Wang Xin <xinwang614@gmail.com>
2022-12-28 14:57:14 +0800

    add base64.cpp/h

--- init.h
+++ init.h
@@ -3,13 +3,13 @@
 
 #include <string>
 
-std::string __appname__{"labelmeplus"};
+inline std::string __appname__{"labelmeplus"};
 
 /* Semantic Versioning 2.0.0: https://semver.org/
  * 1. MAJOR version when you make incompatible API changes;
  * 2. MINOR version when you add functionality in a backwards-compatible manner;
  * 3. PATCH version when you make backwards-compatible bug fixes.
  */
-std::string __version__{"5.1.1"};
+inline std::string __version__{"5.1.1"};
 
 #endif // INIT_H
