d8e74729eb17032289248fc4cf0ad336dfa011e1
Wang Xin <xinwang614@gmail.com>
2023-02-13 23:55:04 +0800

    add FileDialogPreview class

---
+++ widgets/file_dialog_preview.h
@@ -0,0 +1,29 @@
+#ifndef FILEDIALOGPREVIEW_H
+#define FILEDIALOGPREVIEW_H
+
+#include <QFileDialog>
+#include <QLabel>
+#include <QScrollArea>
+
+class ScrollAreaPreview : public QScrollArea {
+ public:
+  ScrollAreaPreview(QWidget* parent = nullptr);
+  QLabel* label{};
+  void setText(const QString& text);
+  void setPixmap(const QPixmap& pixmap);
+  void clear();
+};
+
+class FileDialogPreview : public QFileDialog {
+  Q_OBJECT
+ public:
+  FileDialogPreview(QWidget* parent = nullptr,
+                    const QString& caption = QString(),
+                    const QString& directory = QString(),
+                    const QString& filter = QString());
+  ScrollAreaPreview* labelPreview{};
+ private slots:
+  void onChange(const QString& path);
+};
+
+#endif  // FILEDIALOGPREVIEW_H
